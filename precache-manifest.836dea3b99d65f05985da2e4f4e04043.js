ff601c2a1e836c0306d38d139c2c101474bae4fe
Marius Lux <109797990+luxmarius@users.noreply.github.com>
2024-02-28 13:04:49 +0100

    Updates

---
+++ precache-manifest.836dea3b99d65f05985da2e4f4e04043.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e80b34a3222e4e9b1ec53e8ba3d9ff",
+    "url": "/https//luxmarius.github.io/leape-foerderberater/index.html"
+  },
+  {
+    "revision": "78af9653d918ad3f24af",
+    "url": "/https//luxmarius.github.io/leape-foerderberater/static/css/2.68e2b8a9.chunk.css"
+  },
+  {
+    "revision": "1abfbf143886b75e504f",
+    "url": "/https//luxmarius.github.io/leape-foerderberater/static/css/main.da2d1136.chunk.css"
+  },
+  {
+    "revision": "78af9653d918ad3f24af",
+    "url": "/https//luxmarius.github.io/leape-foerderberater/static/js/2.6fae3a07.chunk.js"
+  },
+  {
+    "revision": "7815f73401c1ee46795006ca46f7b234",
+    "url": "/https//luxmarius.github.io/leape-foerderberater/static/js/2.6fae3a07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1abfbf143886b75e504f",
+    "url": "/https//luxmarius.github.io/leape-foerderberater/static/js/main.be0d887e.chunk.js"
+  },
+  {
+    "revision": "4d9bb9a2745526330330",
+    "url": "/https//luxmarius.github.io/leape-foerderberater/static/js/runtime-main.8cf85854.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/https//luxmarius.github.io/leape-foerderberater/precache-manifest.836dea3b99d65f05985da2e4f4e04043.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/https//luxmarius.github.io/leape-foerderberater/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
